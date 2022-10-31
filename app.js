a1f9076d123092c21d74419c1e8b4071bc9eae7c
LeonID <lmatskevich79@gmail.com>
2022-11-01 01:37:01 +0300

    refactor: using BEM in accordance with rule 2.1 html-and-css-extended

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var createNewTaskElement=function(taskString){
 
   label.innerText=taskString;
   label.className="task";
-  listItem.className="item";
+  listItem.className="list__item";
 
 //Each elements, needs appending
   checkBox.type="checkbox";
