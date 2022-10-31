6f0216de29886c82fb8cbe385c2c0c06aaa82b48
LeonID <lmatskevich79@gmail.com>
2022-11-01 00:34:29 +0300

    refactor: fixed tag selectors according to rule 3.4 in html-and-css

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ var createNewTaskElement=function(taskString){
 
   label.innerText=taskString;
   label.className="task";
+  listItem.className="item";
 
 //Each elements, needs appending
   checkBox.type="checkbox";
